d28e46a6cf1af928025fd9b62980c41633de1ce0
Pranay Jain <jainpranay20@gmail.com>
2025-03-04 00:37:28 +0530

    Setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// Specifies where Tailwind should look for class names in your project. content configuration
